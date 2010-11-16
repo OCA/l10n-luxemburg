715b8fd3a172d1abea6cbaf4c6a7b97482432f00
Xavier ALT <xavier@thamini.com>
2010-11-16 14:55:22 +0100

    [IMP] reworked original module with type str,int,float,ref,python

---
+++ core_config/__init__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    core_config module for OpenERP, Core Configuration Options
+#    Copyright (C) 2010 Thamini S.à.R.L (<http://www.thamini.com>) Xavier ALT
+#
+#    This file is a part of core_config
+#
+#    core_config is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    core_config is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import ir_config
---
+++ core_config/__terp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    core_config module for OpenERP, Core Configuration Options
+#    Copyright (C) 2010 Thamini S.à.R.L (<http://www.thamini.com>) Xavier ALT
+#
+#    This file is a part of core_config
+#
+#    core_config is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    core_config is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : "Core Config",
+    "version" : "0.1",
+    "author" : "Thamini",
+    "category" : "Generic Modules/Others",
+    "website": "http://www.thamini.com",
+    "description": "",
+    "depends" : ['base'],
+    "init_xml" : [
+    ],
+    "update_xml" : [
+        "ir_config_view.xml",
+    ],
+    "active": False,
+    "installable": True
+}
