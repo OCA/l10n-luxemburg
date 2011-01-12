ffa69bf07e996c6c26d8b769f54c3baac9060da7
Xavier ALT <xavier@thamini.com>
2011-01-13 00:35:10 +0100

    [FIX] fix default date from statement line

--- account_payment_import_multiline/account_bank_statement.py
+++ account_payment_import_multiline/account_bank_statement.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import time
 from osv import osv
 from osv import fields
 
@@ -14,6 +15,17 @@ class account_bank_statement_line_i(osv.osv):
     _inherit = 'account.bank.statement.line'
     _order = 'sequence ASC'
     _columns = {
+    }
+
+    def _get_default_date(self, cr, uid, context=None):
+        print("default_date context: %s" % (str(context)))
+        cdate = context.get('date', '')
+        if cdate:
+            return cdate
+        return time.strftime('%Y-%m-%d')
+
+    _defaults = {
+        'date': _get_default_date,
     }
 
 account_bank_statement_line_i()
