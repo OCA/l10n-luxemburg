fbd01f57b1f0054384989f62e0d534cddedd6991
Xavier ALT <xavier@thamini.com>
2011-01-18 07:51:38 +0100

    [FIX] remove part moved into account_optimization module

--- account_payment_import_multiline/account_bank_statement.py
+++ account_payment_import_multiline/account_bank_statement.py
@@ -11,23 +11,3 @@ class account_bank_statement_i(osv.osv):
     }
 account_bank_statement_i()
 
-class account_bank_statement_line_i(osv.osv):
-    _inherit = 'account.bank.statement.line'
-    _order = 'sequence ASC'
-    _columns = {
-    }
-
-    def _get_default_date(self, cr, uid, context=None):
-        print("default_date context: %s" % (str(context)))
-        cdate = context.get('date', '')
-        if cdate:
-            return cdate
-        return time.strftime('%Y-%m-%d')
-
-    _defaults = {
-        'date': _get_default_date,
-    }
-
-account_bank_statement_line_i()
-
-
