76bee43f7d4cfcdd975fa71b22ce93de9f788443
Xavier ALT <xavier@thamini.com>
2011-04-07 23:30:27 +0200

    [FIX] do not fail on matching, if no data after postalcode

--- account_payment_import_multiline/import_multiline.py
+++ account_payment_import_multiline/import_multiline.py
@@ -133,8 +133,13 @@ class account_bank_statement_mt940_import_wizard(osv.osv_memory):
                     values['beneficiary'] = '%s %s %s' % (data[:pos], group, data[posr:])
                 elif not data[pos-1].isspace() and data[posr].isspace():
                     values['beneficiary'] = '%s %s%s' % (data[:pos], group, data[posr:])
-                elif data[pos-1].isspace() and not data[posr].isspace():
-                    values['beneficiary'] = '%s%s %s' % (data[:pos], group, data[posr:])
+                elif data[pos-1].isspace():
+                    if posr >= len(data):
+                        data_posr = ''
+                    else:
+                        data_posr = data[posr:]
+                    if len(data_posr) and not data_posr[0].isspace():
+                        values['beneficiary'] = '%s%s %s' % (data[:pos], group, data_posr)
                 return
         values['_beneficiary'] = data
 
