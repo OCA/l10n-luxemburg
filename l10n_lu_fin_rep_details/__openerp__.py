8139e54534e16aea6349b6eb49b9e7e57d39c663
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-10-29 15:47:34 +0100

    [FIX] headers and license, end of files

--- l10n_lu_fin_rep_details/__openerp__.py
+++ l10n_lu_fin_rep_details/__openerp__.py
@@ -3,14 +3,6 @@
 #
 #    Authors: Laetitia Gangloff
 #    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
-#    All Rights Reserved
-#
-#    WARNING: This program as such is intended to be used by professional
-#    programmers who take the whole responsibility of assessing all potential
-#    consequences resulting from its eventual inadequacies and bugs.
-#    End users who are looking for a ready-to-use solution with commercial
-#    guarantees and support are strongly advised to contact a Free Software
-#    Service Company.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -31,11 +23,10 @@
     "name": "Luxembourg - Accounting - Financial Report Details",
     "version": "0.1",
     "author": "ACSONE SA/NV",
-    "license": "GPL-3",
+    "license": "AGPL-3",
     "category": "Accounting & Finance",
     "website": "http://acsone.eu",
     "depends": ["l10n_lu_ext"],
-    "module": "",
     "description": """
     Luxemburg financial report details
 
@@ -52,5 +43,3 @@
     "active": False,
     "installable": True
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
