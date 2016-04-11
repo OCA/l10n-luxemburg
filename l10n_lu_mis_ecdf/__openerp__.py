36382ff504f4488565c83e3bb869ae5b7b566c42
Thomas Binsfeld <thomas.binsfeld@gmail.com>
2016-04-11 09:43:46 +0200

    [FIX] Correction of descriptor file

--- l10n_lu_mis_ecdf/__openerp__.py
+++ l10n_lu_mis_ecdf/__openerp__.py
@@ -1,17 +1,17 @@
 {
     "name": "Luxembourg - Accounting - MIS - eCDF",
-    "version": "1",
+    "version": "8.0.1.0.0",
     "author": "ACSONE SA/NV",
     "license": "Other proprietary",
     "category": "Accounting & Finance",
     "website": "http://acsone.eu",
-    "depends": ["l10n_lu_ext"],
+    "depends": ["l10n_lu_ext",
+                "l10n_lu_mis_reports"],
     "module": "",
-    "description": "Génération rapports eCDF avec MIS Builder",
+    "summary": "Generates eCDF financial reports with MIS Builder",
     "data": [
         "views/res_company.xml",
         "wizard/ecdf_mis_report_view.xml",
     ],
     "installable": True,
-    "auto_install": False,
 }
