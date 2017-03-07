9c2bd46aec20b351990123854ea66ba1228e5f90
gilles <meyomesse.gilles@gmail.com>
2017-03-07 12:27:36 +0100

    [MIG] l10n_lu_ext from v8 to v10

--- l10n_lu_ext/models/res_company.py
+++ l10n_lu_ext/models/res_company.py
@@ -1,29 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# This file is part of l10n_lu_ext,
-# an Odoo module.
-#
-# Authors: ACSONE SA/NV (<http://acsone.eu>)
-#
-# l10n_lu_ext is free software:
-# you can redistribute it and/or modify it under the terms of the GNU
-# Affero General Public License as published by the Free Software
-# Foundation,either version 3 of the License, or (at your option) any
-# later version.
-#
-# l10n_lu_ext is distributed
-# in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-# PURPOSE. See the GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with l10n_lu_ext.
-# If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
